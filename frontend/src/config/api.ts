927f582868c030cfb0cc1cdcd493f400e50eb8b3
Alb9ert <alberthansen2800@gmail.com>
2026-02-18 18:35:59 +0100

    save

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://recipe-pwa-i79y.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
